a6c60322224014d948def483af47d5bdc794ae16
Rajas Chitale <rajas@Rajass-MBP.lan>
2024-10-16 19:30:08 -0700

    changed argument to run on cifar10

--- distillation/gen_prototype_imgnt.sh
+++ distillation/gen_prototype_imgnt.sh
@@ -1,9 +1,9 @@
 CUDA_VISIBLE_DEVICES=0 python gen_prototype.py \
     --batch_size 10 \
-    --data_dir ../data/imagenet \
-    --dataset imagenet \
-    --diffusion_checkpoints_path ../stablediffusion/checkpoints/stable-diffusion-v1-5 \
+    --data_dir ../../cifar10 \
+    --dataset cifar10 \
+    --diffusion_checkpoints_path stable-diffusion-v1-5/stable-diffusion-v1-5 \
     --ipc 10 \
     --km_expand 1 \
-    --label_file_path ./label-propmt/imagenet_classes.txt \
+    --label_file_path ./label-prompt/CIFAR-10_labels.txt \
     --save_prototype_path ./prototypes
