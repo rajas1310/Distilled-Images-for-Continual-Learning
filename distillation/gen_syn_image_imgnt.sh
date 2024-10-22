bf54bdb391763f71110d9b8e94c75967d6f22c27
rajas1310 <rajaschitale@gmail.com>
2024-10-21 18:00:35 -0700

    added a shell script to setup the environment

--- distillation/gen_syn_image_imgnt.sh
+++ distillation/gen_syn_image_imgnt.sh
@@ -1,10 +1,10 @@
 CUDA_VISIBLE_DEVICES=0 python gen_syn_image.py \
-    --dataset imagenet \
-    --diffusion_checkpoints_path ../stablediffusion/checkpoints/stable-diffusion-v1-5 \
+    --dataset cifar10 \
+    --diffusion_checkpoints_path stable-diffusion-v1-5/stable-diffusion-v1-5 \
     --guidance_scale 8 \
     --strength 0.7 \
     --ipc 10 \
     --km_expand 1 \
-    --label_file_path ./label-propmt/imagenet_classes.txt \
-    --prototype_path ./prototypes/imagenet-ipc10-kmexpand1.json \
+    --label_file_path /home1/rachital/rajas/D4M/distillation/label-prompt/CIFAR-10_labels.txt \
+    --prototype_path ./prototypes/cifar10-ipc10-kmexpand1.json \
     --save_init_image_path ../data/distilled_data/
