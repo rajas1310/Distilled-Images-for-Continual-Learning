28cef6a444b5b3cb6988f569337fd53d9c06b974
rajas1310 <rajaschitale@gmail.com>
2024-10-22 09:17:43 -0700

    save taskwise prototype files and syn images

--- distillation/gen_prototype_imgnt.sh
+++ distillation/gen_prototype_imgnt.sh
@@ -7,7 +7,7 @@ CUDA_VISIBLE_DEVICES=0 python gen_prototype.py \
     --data_dir ../../cifar10 \
     --dataset cifar10 \
     --diffusion_checkpoints_path stable-diffusion-v1-5/stable-diffusion-v1-5 \
-    --ipc 10 \
+    --ipc 20 \
     --km_expand 1 \
-    --label_file_path ./label-prompt/CIFAR-10_labels.txt \
-    --save_prototype_path ./prototypes
+    --save_prototype_path ./prototypes \
+    --task 0
--- distillation/gen_syn_image_imgnt.sh
+++ distillation/gen_syn_image_imgnt.sh
@@ -3,8 +3,8 @@ CUDA_VISIBLE_DEVICES=0 python gen_syn_image.py \
     --diffusion_checkpoints_path stable-diffusion-v1-5/stable-diffusion-v1-5 \
     --guidance_scale 8 \
     --strength 0.7 \
-    --ipc 10 \
+    --ipc 20 \
     --km_expand 1 \
-    --label_file_path /home1/rachital/rajas/D4M/distillation/label-prompt/CIFAR-10_labels.txt \
     --prototype_path ./prototypes/cifar10-ipc10-kmexpand1.json \
-    --save_init_image_path ../data/distilled_data/
+    --save_init_image_path ../data/distilled_data/ \
+    --task 0
