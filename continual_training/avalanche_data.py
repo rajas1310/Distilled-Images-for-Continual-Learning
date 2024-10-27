2f8acbcfeeece264638e3f99bafae0b1a2b72e66
mengyuan2208 <meng.yuan.2208@gmail.com>
2024-10-26 22:42:39 -0700

    Add code for CustomSyntheticDataset and training with synthetic images.

--- continual_training/avalanche_data.py
+++ continual_training/avalanche_data.py
@@ -2,9 +2,12 @@ from torchvision import datasets, transforms
 # from avalanche.benchmarks.generators import nc_benchmark, ni_benchmark
 from avalanche.benchmarks.scenarios.deprecated.generators import nc_benchmark
 import torch
-from transformers import ViTModel
 import torch.nn as nn
-
+import os
+from torchvision.datasets import DatasetFolder
+from torchvision.io import read_image
+from torch.utils.data import Subset
+import random
 class ResNet(nn.Module):
     def __init__(self, args, model_name='resnet18'):
         super().__init__()
@@ -32,7 +35,80 @@ class ResNet(nn.Module):
         )
 
 class CustomSyntheticDataset():
-    pass
+    def __init__(self, args):
+        self.args = args
+
+        if args.dataset == 'cifar10':
+            image_mean = (0.491, 0.482, 0.447)
+            image_std = (0.202, 0.199, 0.201)
+        elif args.dataset == 'mnist':
+            image_mean = (0.131,)
+            image_std = (0.308,)
+        elif args.dataset == 'imagenet':
+            image_mean = (0.485, 0.456, 0.406)
+            image_std = (0.229, 0.224, 0.225)
+
+        self.transform =  transforms.Compose([
+                                transforms.Normalize(mean=image_mean, std=image_std)
+                                ]) 
+        self.set_data_variables()
+    
+    def load_image_as_tensor(self, path):
+        return read_image(path).float() / 255.0  # Normalize pixel values to [0, 1]
+
+    def set_data_variables(self):
+        if self.args.dataset == 'cifar10':
+            self.task_dict = {0:2, 1:2, 2:2, 3:2, 4:2}
+
+            self.train_datasets = []
+            self.test_datasets = []
+
+            train_ratio = 0.8
+
+            for stage in range(len(self.task_dict)):
+                stage_dir = os.path.join(self.args.data_dir, f"task{stage}")
+                
+                full_dataset = DatasetFolder(
+                root=stage_dir,
+                loader=self.load_image_as_tensor,
+                extensions=("png"),
+                transform=self.transform
+                )
+                class_indices = {}
+                for idx, (_, label) in enumerate(full_dataset):
+                    if label not in class_indices:
+                        class_indices[label] = []
+                    class_indices[label].append(idx)
+                train_indices = []
+                test_indices = []
+                for label, indices in class_indices.items():
+                    random.shuffle(indices)
+                    split_point = int(len(indices) * train_ratio)
+                    train_indices.extend(indices[:split_point])
+                    test_indices.extend(indices[split_point:])
+                trainset = Subset(full_dataset, train_indices)
+                testset = Subset(full_dataset, test_indices)
+
+                self.train_datasets.append(trainset)
+                self.test_datasets.append(testset)
+                
+            self.trainset = torch.utils.data.ConcatDataset(self.train_datasets)
+            self.testset = torch.utils.data.ConcatDataset(self.test_datasets)
+
+        elif self.args.dataset == 'mnist':
+            pass
+
+        elif self.args.dataset == 'imagenet':
+            pass
+        
+
+
+    def get_scenario(self):
+        scenario = nc_benchmark(
+            self.train_datasets, self.test_datasets, n_experiences=len(self.task_dict), per_exp_classes=self.task_dict, shuffle=False, task_labels=True
+        )
+        print("Trainset : ", len(self.trainset), "Testset : ", len(self.testset))
+        return scenario
 
 class CustomOriginalDataset():
     def __init__(self, args):
---
+++ continual_training/avalanche_train_synthetic.py
@@ -0,0 +1,117 @@
+import torch
+import torch.nn as nn
+
+import numpy as np
+from avalanche.training.supervised import Replay, AGEM, EWC  # and many more!
+from avalanche.logging import TextLogger, InteractiveLogger
+from avalanche.training.plugins import EvaluationPlugin
+from avalanche.evaluation.metrics import forgetting_metrics, accuracy_metrics,\
+    loss_metrics, timing_metrics, cpu_usage_metrics, StreamConfusionMatrix,\
+    disk_usage_metrics, gpu_usage_metrics
+
+from avalanche.checkpointing import maybe_load_checkpoint, save_checkpoint
+
+from avalanche_data import CustomSyntheticDataset, ResNet
+
+import argparse
+
+import sys, os
+from logger_utils import Logger
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-e', '--epochs', type=int, default=50)
+parser.add_argument('-bs', '--batch-size', type=int, default=16)
+parser.add_argument('-lr', '--lr', type=float, default=5e-6)
+parser.add_argument('-wd', '--weight-decay', type=float, default=1e-6)
+# parser.add_argument('-p', '--patience', type=int, default=10)
+parser.add_argument('-nw','--num-workers', type=int, default=0)
+# parser.add_argument('--test-interval', type=int, default=1)
+parser.add_argument('--device', type=str, default="cpu")
+# parser.add_argument('--seed', type=int, default=42)
+
+parser.add_argument('--strategy', type=str, default="replay")
+
+parser.add_argument('-d', '--dataset', type=str, default='cifar10')
+parser.add_argument('-ddir', '--data-dir', type=str, default='../../data')
+parser.add_argument('-odir', '--output-dir', type=str, default='./output')
+args = parser.parse_args()
+
+parser.add_argument('-nc', '--num-classes', type=int, default=None)
+args = parser.parse_args()
+
+
+if args.num_classes == None:
+    if args.dataset == 'mnist' or args.dataset == 'cifar10':
+        args.num_classes = 10
+    elif args.dataset == 'imagenet':
+        args.num_classes = 1000
+    elif args.dataset == 'stanfordcars':
+        args.num_classes = 196
+
+print(args, '\n')
+
+if not os.path.exists(args.output_dir):
+    os.makedirs(args.output_dir, exist_ok=True)
+    
+# sys.stdout = Logger(os.path.join(args.output_dir, 'logs-{}-{}-{}.txt'.format(args.dataset, args.strategy, args.epochs)))
+
+model = ResNet(args)
+scenario = CustomSyntheticDataset(args).get_scenario()
+
+text_logger = TextLogger(
+    open(os.path.join(args.output_dir, f'log_{args.dataset}_{args.strategy}_epoch_{args.epochs}_lr_{args.lr}_wd_{args.weight_decay}.txt'), 'a')
+)
+interactive_logger = InteractiveLogger()
+
+eval_plugin = EvaluationPlugin(
+    accuracy_metrics(minibatch=False, epoch=True, experience=True, stream=True),
+    loss_metrics(minibatch=False, epoch=True, experience=True, stream=True),
+    # timing_metrics(epoch=True),
+    # cpu_usage_metrics(experience=True),
+    forgetting_metrics(experience=True, stream=True),
+    # StreamConfusionMatrix(num_classes=args.num_classes, save_image=False),
+    # disk_usage_metrics(minibatch=True, epoch=True, experience=True, stream=True),
+    loggers=[text_logger, interactive_logger]
+)
+
+optimizer = torch.optim.Adam(
+            params=model.parameters(), lr=args.lr, weight_decay=args.weight_decay
+        )
+criterion = nn.CrossEntropyLoss()
+
+if args.strategy == 'replay':
+    cl_strategy = Replay(
+        model, optimizer, criterion,
+        train_mb_size=args.batch_size, train_epochs=args.epochs, eval_mb_size=args.batch_size, evaluator=eval_plugin, device=args.device
+    )
+elif args.strategy == 'agem':
+    cl_strategy = AGEM(
+        model, optimizer, criterion,
+        train_mb_size=args.batch_size, train_epochs=args.epochs, eval_mb_size=args.batch_size, evaluator=eval_plugin, device=args.device,
+        patterns_per_exp = 100
+    )
+elif args.strategy == 'ewc':
+    cl_strategy = EWC(
+        model, optimizer, criterion,
+        train_mb_size=args.batch_size, train_epochs=args.epochs, eval_mb_size=args.batch_size, evaluator=eval_plugin, device=args.device
+    )
+else:
+    ValueError()
+
+
+# TRAINING LOOP
+print('Starting experiment...')
+results = []
+for experience in scenario.train_stream:
+    print("Start of experience: ", experience.current_experience)
+    print("Current Classes: ", experience.classes_in_this_experience)
+
+    res = cl_strategy.train(experience, num_workers = args.num_workers)
+    print('Training completed')
+
+    # save_checkpoint(cl_strategy, f'{args.output_dir}/checkpt_{args.dataset}_task{len(results)}_eps{args.epochs}.pkl')
+
+    print('Computing accuracy on the whole test set')
+    results.append(cl_strategy.eval(scenario.test_stream))
+    print(results[-1])
