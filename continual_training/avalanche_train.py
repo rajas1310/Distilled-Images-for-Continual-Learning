7101d5ebe113f80111f1db909f69de2a3aca350a
rajas1310 <rajaschitale@gmail.com>
2024-10-29 10:09:29 -0700

    Edited args

--- continual_training/avalanche_train.py
+++ continual_training/avalanche_train.py
@@ -56,12 +56,14 @@ if args.num_classes == None:
 
 print(args, '\n')
 
+args.output_dir = f'{args.output_dir}/{args.model}'
+
 if not os.path.exists(args.output_dir):
-    os.makedirs(f'{args.output_dir}/{args.model}', exist_ok=True)
+    os.makedirs(args.output_dir, exist_ok=True)
     
 # sys.stdout = Logger(os.path.join(args.output_dir, 'logs-{}-{}-{}.txt'.format(args.dataset, args.strategy, args.epochs)))
 timestamp = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-log_filename = os.path.join(args.output_dir, args.model, 'logs-{}-{}-{}-{}.txt'.format(args.dataset, args.strategy, args.epochs, timestamp))
+log_filename = os.path.join(args.output_dir, 'logs-{}-{}-{}-{}.txt'.format(args.dataset, args.strategy, args.epochs, timestamp))
 
 model = ResNet(args)
 scenario = CustomOriginalDataset(args).get_scenario()
