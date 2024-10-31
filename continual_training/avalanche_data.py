e28d3cc7fb8d03640bdd00b4ad5dc24e9d9e94b0
mengyuan2208 <meng.yuan.2208@gmail.com>
2024-10-30 21:00:54 -0700

    Fixed bug for loading synthetic dataset.

--- continual_training/avalanche_data.py
+++ continual_training/avalanche_data.py
@@ -54,49 +54,21 @@ class CustomSyntheticDataset():
                     transforms.ToTensor(),
                     transforms.Normalize(mean=image_mean, std=image_std)
                     ])
-        self.task_dict = {
-            'cifar10': {
-                0: ['airplane', 'automobile'],
-                1: ['bird', 'cat'],
-                2: ['deer', 'dog'],
-                3: ['frog', 'horse'],
-                4: ['ship', 'truck']
-            }
-        }
         self.set_data_variables()
 
     def set_data_variables(self):
         if self.args.dataset == 'cifar10':
+            self.trainset = datasets.ImageFolder(
+                root=self.args.data_dir,
+                transform=self.train_transform
+            )
 
-            self.trainset = []
-            self.testset = []
-
-            for stage in range(len(self.task_dict['cifar10'])):
-                stage_dir = os.path.join(self.args.data_dir, f"task{stage}")
-                
-                train_dataset = datasets.ImageFolder(
-                    root=stage_dir,
-                    transform=self.train_transform
-                )
-                self.trainset.append(train_dataset)
-
-            cifar10_class_to_idx = {
-                'airplane': 0, 'automobile': 1, 'bird': 2, 'cat': 3,
-                'deer': 4, 'dog': 5, 'frog': 6, 'horse': 7, 'ship': 8, 'truck': 9
-            }
-
-            full_test_dataset = datasets.CIFAR10(
-                root=self.args.data_dir, train=False, download=True,
+            self.testset = datasets.CIFAR10(
+                root="../../cifar10", train=False, download=True,
                 transform=self.test_transform
             )
-            for stage, class_names in self.task_dict['cifar10'].items():
-                class_indices = [cifar10_class_to_idx[class_name] for class_name in class_names]
-                test_indices = [i for i, (_, label) in enumerate(full_test_dataset) if label in class_indices]
-                remapped_labels = torch.tensor([class_indices.index(full_test_dataset.targets[i]) for i in test_indices])
-                test_subset = Subset(full_test_dataset, test_indices)
-                test_subset.targets = remapped_labels
-                print(f"Stage {stage}, Classes: {class_names}, Unique Labels in Test Subset: {set(test_subset.targets.tolist())}")
-                self.testset.append(test_subset)
+
+            self.task_dict = {0:2, 1:2, 2:2, 3:2, 4:2}
 
         elif self.args.dataset == 'mnist':
             pass
@@ -110,10 +82,8 @@ class CustomSyntheticDataset():
             print("Trainset : ", len(self.trainset), "Testset : ", len(self.testset))
             scenario = nc_benchmark(
                 self.trainset, self.testset, 
-                n_experiences=len(self.task_dict['cifar10']),
-                one_dataset_per_exp = True,
-                class_ids_from_zero_in_each_exp = True,
-                # per_exp_classes={i: len(classes) for i, classes in self.task_dict['cifar10'].items()},
+                n_experiences=len(self.task_dict),
+                per_exp_classes=self.task_dict,
                 shuffle=False,
                 task_labels=True
             )
